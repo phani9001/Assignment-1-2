dfacef84ef79f35c9842ef74b3bfac933f01833a
phani9001 <143939273+phani9001@users.noreply.github.com>
2025-07-11 10:53:09 +0530

    Delete Assignment 1&2 directory

--- Assignment 1&2/Assign_1/Assign_1.py
+++
@@ -1,6 +0,0 @@
-num_1 =int(input("Enter first number:"))
-num_2 =int(input("Enter second number:"))
-print(f"Addition: {num_1+num_2}")
-print(f"Subtraction:{num_1-num_2}")
-print(f"Multiplication:{num_1*num_2}")
-print(f"Division:{num_1%num_2}")
