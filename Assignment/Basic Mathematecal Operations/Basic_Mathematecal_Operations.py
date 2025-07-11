bf2d72efa9eadb9d17108b67eb20703cd23905ac
phani9001 <143939273+phani9001@users.noreply.github.com>
2025-07-11 10:54:38 +0530

    Add files via upload

---
+++ Assignment/Basic Mathematecal Operations/Basic_Mathematecal_Operations.py
@@ -0,0 +1,7 @@
+num_1 =int(input("Enter first number:"))
+num_2 =int(input("Enter second number:"))
+print(f"Addition: {num_1+num_2}")
+print(f"Subtraction:{num_1-num_2}")
+print(f"Multiplication:{num_1*num_2}")
+print(f"Division:{num_1%num_2}")
+
---
+++ Assignment/Create a Personalized Greeting/Create_a_Personalized_Greeting.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+user = first_name +" "+ last_name
+print(f"Hello, {user}! welcome to the python program")
